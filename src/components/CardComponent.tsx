c5b7821d2cbea63e7176dd1e5dedfc3d9980c757
Jon Paul Simonelli <jonpaul.simonelli@gmail.com>
2023-01-15 14:49:02 -0500

    Add event handler to available CardComponent props.

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,13 +1,16 @@
+import React from 'react';
 import Card from '../models/Card';
 
 interface CardProps {
     card: Card,
-    deckType?: string
+    deckType?: string,
+    handleClick: (event: React.MouseEvent<HTMLElement>) => void,
+    isSelected: boolean
 }
 
-const CardComponent = ({card, deckType='french-trad'}: CardProps):JSX.Element => {
+const CardComponent = ({card, handleClick, deckType='french-trad', isSelected=false}: CardProps):JSX.Element => {
     return (
-        <div className='card-container'>
+        <div className='card-container' onClick={handleClick}>
             <img src={require(`../assets/images/decks/${deckType}/${card.imageURI}`)} alt={card.imageURI}></img>
         </div>
     );
